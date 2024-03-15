cf0eeffdd0348a083a3820c8b75a53294ca527e0
David Ramírez <cdavidramirezg24@gmail.com>
2024-03-14 23:41:27 -0300

    database connection

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,31 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
+  app.setGlobalPrefix('api/');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Matriz de riesgos')
+    .setDescription('Desarrollo backend para matriz de riesgos operacionales')
+    .setVersion('1.0')
+    .setContact('David Ramirez', '', 'cdavidamirezg24@gmail.com')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT);
+  logger.log(`App running on port ${process.env.PORT}`);
 }
 bootstrap();
